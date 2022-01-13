f387d3141b3730ed2bf46898a43e49a1d81763e8
AhasanulIslam <bsse1024@iit.du.ac.bd>
2022-01-13 15:34:43 +0600

    new

--- object.js
+++ object.js
@@ -2,7 +2,13 @@ const person = {
     firstName: "John",
     lastName: "Doe",
     age: 50,
-    eyeColor: "blue"
+    eyeColor: "blue",
+
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
   };
 
   console.log(person.firstName + " is " + person.age + " years old.");
+
+  console.log(person.fullName());
