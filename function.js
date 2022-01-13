295578606224c61d37105412fdf4f59520514c3c
AhasanulIslam <bsse1024@iit.du.ac.bd>
2022-01-13 09:45:58 +0600

    operation

--- function.js
+++ function.js
@@ -1,1 +1,27 @@
-console.log(24);
+function sum(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+let a = sum(5, 7);
+console.log(a);
+
+let b = sub(5, 7);
+console.log(b);
+
+let c = mul(5, 7);
+console.log(c);
+
+let d = div(5, 7);
+console.log(d);
